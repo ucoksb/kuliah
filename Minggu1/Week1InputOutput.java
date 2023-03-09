47bc906d6623d1b6b1933372b6e77c93d1ed174d
ucoksb <ucokb97288@gmail.com>
2023-03-10 03:55:47 +0700

    Minggu1

---
+++ Minggu1/Week1InputOutput.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Week1InputOutput {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int number = input.nextInt();
+        System.out.print("You entered " + number);
+
+        // closing the scanner object
+        input.close();
+    }
+}
