aae02c3f7621010bf0bdcf620d53ec4685e0ef18
ucoksb <ucokb97288@gmail.com>
2023-03-10 03:47:07 +0700

    Main

--- Minggu2/Challange1.java
+++ Minggu2/Challange1.java
@@ -3,10 +3,11 @@ package Minggu2;
 import java.util.Scanner;
 
 public class Challange1 {
-    public static void main(System[] args) {
-        String nama;
+    public static void main(String[] args) {
+        String grade, nama;
         int FEScore, MEScore, AScore;
-        
+        final double sum_value;
+
         Scanner keyboard = new Scanner(System.in);
 
         System.out.print("Enter your name : ");
@@ -17,11 +18,30 @@ public class Challange1 {
         MEScore = keyboard.nextInt();
         System.out.print("Enter Assignment Score = ");
         AScore = keyboard.nextInt();
-        double sum_value = (0.40 * FEScore + 0.35 * MEScore + 0.25 * AScore);
+        sum_value = (0.40 * FEScore + 0.35 * MEScore + 0.25 * AScore);
 
-        System.out.println(nama+" NIlai akhirnya adalah "+sum_value);
+        if (sum_value >= 85 && sum_value < 100) {
+            grade = "A";
+
+        } else if (sum_value >= 80 && sum_value < 85) {
+            grade = "A-";
+        } else if (sum_value >= 75 && sum_value < 80) {
+            grade = "B+";
+        } else if (sum_value >= 80 && sum_value < 85) {
+            grade = "B";
+        } else if (sum_value >= 75 && sum_value < 80) {
+            grade = "B-";
+        } else if (sum_value >= 60 && sum_value < 75) {
+            grade = "C+";
+        } else if (sum_value >= 50 && sum_value < 60) {
+            grade = "C";
+        } else if (sum_value >= 40 && sum_value < 50) {
+            grade = "D";
+        } else {
+            grade = "E";
+        }
+        System.out.print("\nHi " + nama + "\nyour final score is " + grade);
 
         keyboard.close();
     }
-
 }
