a543b276280d7cf97b56172f9bbfabad97c84d91
ucoksb <ucokb97288@gmail.com>
2023-03-17 00:11:10 +0700

    Main

---
+++ PBO/Minggu3/MobilBeraksi.java
@@ -0,0 +1,13 @@
+package Minggu3;
+
+public class MobilBeraksi{
+    public static void main(String[] arggs){
+    Mobil mobilku = new Mobil();
+    
+    /* memanggil atribut dan memberi nilat */
+    mobilku.warna = "Hitam";
+    mobilku.tahunProduksi = 2006;
+    System.out.println("Warna: " + mobilku.warna);
+    System.out.println("Tahun: " + mobilku.tahunProduksi);
+    }
+}
